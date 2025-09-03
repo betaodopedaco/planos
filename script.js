e3b285b6d0f7e079566da9341b988408d4e17a69
nicolas emanuel <brancosupremo12@gmail.com>
2025-09-02 23:58:58 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Este arquivo está vazio pois o código original não continha JavaScript
+// Você pode adicionar funcionalidades JavaScript aqui no futuro
+
+// Exemplo de código que você pode adicionar:
+// document.addEventListener('DOMContentLoaded', function() {
+//   console.log('Site carregado!');
+// });
